6eefa1b6d53a50ac6af94040abd2ff293dc4ef23
Daniel <danieldev2310@gmail.com>
2022-10-27 21:32:05 +0300

    style

--- ui.py
+++ ui.py
@@ -2,13 +2,20 @@ from json import load
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-st.title('Knesset Tweets')
+st.set_page_config(
+   page_title="Knesset Tweets",
+   page_icon="🧊",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
 data = pd.read_csv('tfidf.csv',index_col='faction')
 names = data.columns
 
 if 'names' not in st.session_state:
     st.session_state.names = []
+
+if 'filter' not in st.session_state:
+    st.session_state.filter = ''
 
 
 filtered = data.filter(items=st.session_state.names)
@@ -16,16 +23,17 @@ filtered = data.filter(items=st.session_state.names)
 # selected_terms = st.multiselect(
 #     'Select term to measure',
 #     data.columns)
+st.session_state.filter = st.sidebar.text_input('Label to filter')
 
 def update_terms(_n):
-    print(st.session_state.names,_n)
     if _n not in st.session_state.names:
         st.session_state.names.append(_n)
     else:
         st.session_state.names.remove(_n)
 
 for name in names:
-    st.sidebar.checkbox(name,on_change=update_terms,args=(name,))
+    if not len(st.session_state.filter) or name in st.session_state.filter:
+        st.sidebar.checkbox(name, value=name in st.session_state.names,on_change=update_terms,args=(name,))
 
 st.bar_chart(data=filtered)
-st.write(filtered)
+st.table(filtered)
